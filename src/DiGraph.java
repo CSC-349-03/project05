1f4e951e4e8954b36fc3d1e2b777f308197f8eab
Andrew LeDawson <andrew@andrewiam.com>
2018-03-07 11:38:22 -0800

    Initial commit

---
+++ src/DiGraph.java
@@ -0,0 +1,39 @@
+/*  David Twyman, Andrew LeDawson
+ **  dtwyman@calpoly.edu, aledawson@calpoly.edu
+ **  CSC 349-03
+ **  Project 5
+ **  03-09-2018
+ */
+
+import java.util.LinkedList;
+
+public class DiGraph {
+    LinkedList[] graph;
+
+    public DiGraph(int N){
+        graph = new LinkedList[N];
+    }
+
+    public void addEdge(int from, int to){
+        // TODO: Search for edge
+        // TODO: Add edge if not found
+    }
+
+    public void deleteEdge(int from, int to){
+        // TODO
+    }
+
+    public int edgeCount(){
+        // TODO
+        return -1;
+    }
+
+    public int vertexCount(){
+        // TODO
+        return -1;
+    }
+
+    public void print(){
+        // TODO
+    }
+}
